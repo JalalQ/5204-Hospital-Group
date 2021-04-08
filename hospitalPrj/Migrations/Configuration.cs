3e599b9ce8962ac570d206c92aab7b62baca18f6
nastaran azani <azaninastaran@gmail.com>
2021-04-08 09:35:52 -0400

    Add project files.

---
+++ hospitalPrj/Migrations/Configuration.cs
@@ -0,0 +1,23 @@
+namespace hospitalPrj.Migrations
+{
+    using System;
+    using System.Data.Entity;
+    using System.Data.Entity.Migrations;
+    using System.Linq;
+
+    internal sealed class Configuration : DbMigrationsConfiguration<hospitalPrj.Models.hospitalDbContext>
+    {
+        public Configuration()
+        {
+            AutomaticMigrationsEnabled = false;
+        }
+
+        protected override void Seed(hospitalPrj.Models.hospitalDbContext context)
+        {
+            //  This method will be called after migrating to the latest version.
+
+            //  You can use the DbSet<T>.AddOrUpdate() helper extension method 
+            //  to avoid creating duplicate seed data.
+        }
+    }
+}
