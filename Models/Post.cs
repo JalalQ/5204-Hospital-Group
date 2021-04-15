f439760c9e26c64e084cf8558a18db51ddc375b0
Navpreet Kaur <navpreetghuman36@gmail.com>
2021-04-15 09:41:52 -0400

    second commit

---
+++ Models/Post.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace team2Geraldton.Models
+{
+    public class Post
+    {
+        public int PostId { get; set; }
+        public string Title { get; set; }
+        public string Type { get; set; }
+        public string Description { get; set; }
+
+    }
+    public class PostDto
+    {
+        public int PostId { get; set; }
+        [DisplayName("Job Title")]
+        public string Title { get; set; }
+        [DisplayName("Job Type")]
+        public string Type { get; set; }
+        [DisplayName("Job Description")]
+        public string Description { get; set; }
+
+    }
+}
---
+++ Models/Staff.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace team2Geraldton.Models
+{
+    public class Staff
+    {
+        //This class describes a staff entity.
+        //It is used for actually generating a database table
+        [Key]
+        public int StaffId { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+        public string Contact { get; set; }
+        public string Position { get; set; }
+    }
+    public class StaffDto
+    {
+        public int StaffId { get; set; }
+        [DisplayName("First Name")]
+        public string FirstName { get; set; }
+        [DisplayName("Last Name")]
+        public string LastName { get; set; }
+        [DisplayName("Email")]
+        public string Email { get; set; }
+        [DisplayName("Contact")]
+        public string Contact { get; set; }
+        [DisplayName("Position")]
+        public string Position { get; set; }
+
+    }
+
+}
