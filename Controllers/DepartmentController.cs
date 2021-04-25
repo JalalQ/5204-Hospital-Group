65fdce9b0c40588b6321ac7ff46f9b862febd140
simranjeetsingh7896 <simranb7896@gmail.com>
2021-04-25 22:04:31 +0530

    simran commit

---
+++ Controllers/DepartmentController.cs
@@ -0,0 +1,190 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using team2Geraldton.Models;
+using System.IO;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Diagnostics;
+using System.Web.Script.Serialization;
+
+namespace team2Geraldton.Controllers
+{
+    public class DepartmentController : Controller
+    {
+        private JavaScriptSerializer jss = new JavaScriptSerializer();
+        private static readonly HttpClient client;
+
+        static DepartmentController()
+        {
+            HttpClientHandler handler = new HttpClientHandler()
+            {
+                AllowAutoRedirect = false
+            };
+            client = new HttpClient(handler);
+            client.BaseAddress = new Uri("https://localhost:44374/api/");
+            client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+        }
+
+        // GET: Department/List
+
+        public ActionResult List()
+        {
+            string url = "departmentdata/getdepartments";
+            HttpResponseMessage response = client.GetAsync(url).Result;
+            if (response.IsSuccessStatusCode)
+            {
+                IEnumerable<DepartmentDto> SelectedDepartments = response.Content.ReadAsAsync<IEnumerable<DepartmentDto>>().Result;
+                return View(SelectedDepartments);
+            }
+            else
+            {
+                return RedirectToAction("Error");
+            }
+        }
+
+        // GET: Department/Details/5
+        public ActionResult Details(int id)
+        {
+
+            string url = "departmentdata/finddepartment/" + id;
+            HttpResponseMessage response = client.GetAsync(url).Result;
+
+
+            if (response.IsSuccessStatusCode)
+            {
+
+                //Put data into Injury data transfer object
+                DepartmentDto SelectedDepartment = response.Content.ReadAsAsync<DepartmentDto>().Result;
+                return View(SelectedDepartment);
+
+            }
+            else
+            {
+                return RedirectToAction("Error");
+            }
+
+        }
+
+
+        // GET: Department/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+
+        // POST: Department/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken()]
+        public ActionResult Create(Department DepartmentInfo)
+        {
+            Debug.WriteLine(DepartmentInfo.DepartmentName);
+            string url = "departmentdata/adddepartment";
+            Debug.WriteLine(jss.Serialize(DepartmentInfo));
+            HttpContent content = new StringContent(jss.Serialize(DepartmentInfo));
+            content.Headers.ContentType = new MediaTypeHeaderValue("application/json");
+            HttpResponseMessage response = client.PostAsync(url, content).Result;
+
+            if (response.IsSuccessStatusCode)
+            {
+
+                int DepartmentId = response.Content.ReadAsAsync<int>().Result;
+                return RedirectToAction("Details", new { id = DepartmentId });
+            }
+            else
+            {
+                return RedirectToAction("Error");
+            }
+        }
+
+
+        // GET: Department/Edit/2
+        public ActionResult Edit(int id)
+        {
+
+            string url = "departmentdata/finddepartment/" + id;
+            HttpResponseMessage response = client.GetAsync(url).Result;
+            if (response.IsSuccessStatusCode)
+            {
+                //Put data into Injury data transfer object
+                DepartmentDto SelectedDepartment = response.Content.ReadAsAsync<DepartmentDto>().Result;
+                return View(SelectedDepartment);
+            }
+            else
+            {
+                return RedirectToAction("Error");
+
+            }
+        }
+
+
+        // POST: Department/Edit/2
+        [HttpPost]
+        [ValidateAntiForgeryToken()]
+        public ActionResult Edit(int id, Department DepartmentInfo)
+        {
+            Debug.WriteLine(DepartmentInfo.DepartmentName);
+            string url = "departmentdata/updatedepartment/" + id;
+            Debug.WriteLine(jss.Serialize(DepartmentInfo));
+            HttpContent content = new StringContent(jss.Serialize(DepartmentInfo));
+            content.Headers.ContentType = new MediaTypeHeaderValue("application/json");
+            HttpResponseMessage response = client.PostAsync(url, content).Result;
+
+            if (response.IsSuccessStatusCode)
+            {
+                return RedirectToAction("Details", new { id = id });
+            }
+            else
+            {
+                return RedirectToAction("Error");
+            }
+        }
+
+
+        // GET: Department/Delete/5
+        [HttpGet]
+        public ActionResult DeleteConfirm(int id)
+        {
+            string url = "departmentdata/finddepartment/" + id;
+            HttpResponseMessage response = client.GetAsync(url).Result;
+            if (response.IsSuccessStatusCode)
+            {
+          
+                DepartmentDto SelectedDepartment = response.Content.ReadAsAsync<DepartmentDto>().Result;
+                return View(SelectedDepartment);
+            }
+            else
+            {
+                return RedirectToAction("Error");
+
+            }
+        }
+
+
+        // POST: Department/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken()]
+        public ActionResult Delete(int id)
+        {
+            string url = "departmentdata/deletedepartment/" + id;
+            HttpContent content = new StringContent("");
+            HttpResponseMessage response = client.PostAsync(url, content).Result;            
+            if (response.IsSuccessStatusCode)
+            {
+                return RedirectToAction("List");
+            }
+            else
+            {
+                return RedirectToAction("Error");
+            }
+        }
+
+        public ActionResult Error()
+        {
+            return View();
+        }
+    }
+}
---
+++ Controllers/VolunteerOpportunityController.cs
@@ -0,0 +1,191 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using team2Geraldton.Models;
+using System.IO;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Diagnostics;
+using System.Web.Script.Serialization;
+
+namespace team2Geraldton.Controllers
+{
+    public class VolunteerOpportunityController : Controller
+    {
+        private JavaScriptSerializer jss = new JavaScriptSerializer();
+        private static readonly HttpClient client;
+
+        static VolunteerOpportunityController()
+        {
+            HttpClientHandler handler = new HttpClientHandler()
+            {
+                AllowAutoRedirect = false
+            };
+            client = new HttpClient(handler);
+            client.BaseAddress = new Uri("https://localhost:44374/api/");
+            client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+        }
+
+
+        // GET: VolunteerOpportunity/List
+        public ActionResult List()
+        {
+            string url = "volunteeropportunitydata/getvolunteeropportunities";
+            HttpResponseMessage response = client.GetAsync(url).Result;
+            if (response.IsSuccessStatusCode)
+            {
+                IEnumerable<VolunteerOpportunityDto> SelectedVolunteerOpportunities = response.Content.ReadAsAsync<IEnumerable<VolunteerOpportunityDto>>().Result;
+                return View(SelectedVolunteerOpportunities);
+            }
+            else
+            {
+                return RedirectToAction("Error");
+            }
+        }
+
+
+        // GET: VolunteerOpportunity/Details/5
+        public ActionResult Details(int id)
+        {
+
+            string url = "volunteeropportunitydata/findvolunteeropportunity/" + id;
+            HttpResponseMessage response = client.GetAsync(url).Result;
+
+
+            if (response.IsSuccessStatusCode)
+            {
+
+                //Put data into Injury data transfer object
+                VolunteerOpportunityDto SelectedVolunteerOpportunity = response.Content.ReadAsAsync<VolunteerOpportunityDto>().Result;
+                return View(SelectedVolunteerOpportunity);
+
+            }
+            else
+            {
+                return RedirectToAction("Error");
+            }
+
+        }
+
+
+        // GET: VolunteerOpportunity/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: VolunteerOpportunity/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken()]
+        public ActionResult Create(VolunteerOpportunity VolunteerOpportunityInfo)
+        {
+            Debug.WriteLine(VolunteerOpportunityInfo.OpportunityName);
+            string url = "volunteeropportunitydata/addvolunteeropportunity";
+            Debug.WriteLine(jss.Serialize(VolunteerOpportunityInfo));
+            HttpContent content = new StringContent(jss.Serialize(VolunteerOpportunityInfo));
+            content.Headers.ContentType = new MediaTypeHeaderValue("application/json");
+            HttpResponseMessage response = client.PostAsync(url, content).Result;
+
+            if (response.IsSuccessStatusCode)
+            {
+
+                int OpportunityId = response.Content.ReadAsAsync<int>().Result;
+                return RedirectToAction("Details", new { id = OpportunityId });
+            }
+            else
+            {
+                return RedirectToAction("Error");
+            }
+
+
+        }
+
+        // GET: VolunteerOpportunity/Edit/5
+        public ActionResult Edit(int id)
+        {
+
+            string url = "volunteeropportunitydata/findvolunteeropportunity/" + id;
+            HttpResponseMessage response = client.GetAsync(url).Result;
+            if (response.IsSuccessStatusCode)
+            {
+                //Put data into Injury data transfer object
+                VolunteerOpportunityDto SelectedVolunteerOpportunity = response.Content.ReadAsAsync<VolunteerOpportunityDto>().Result;
+                return View(SelectedVolunteerOpportunity);
+            }
+            else
+            {
+                return RedirectToAction("Error");
+
+            }
+        }
+
+
+        // POST: VolunteerOpportunity/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken()]
+        public ActionResult Edit(int id, VolunteerOpportunity VolunteerOpportunityInfo)
+        {
+            Debug.WriteLine(VolunteerOpportunityInfo.OpportunityName);
+            string url = "volunteeropportunitydata/updatevolunteeropportunity/" + id;
+            Debug.WriteLine(jss.Serialize(VolunteerOpportunityInfo));
+            HttpContent content = new StringContent(jss.Serialize(VolunteerOpportunityInfo));
+            content.Headers.ContentType = new MediaTypeHeaderValue("application/json");
+            HttpResponseMessage response = client.PostAsync(url, content).Result;
+
+            if (response.IsSuccessStatusCode)
+            {
+                return RedirectToAction("Details", new { id = id });
+            }
+            else
+            {
+                return RedirectToAction("Error");
+            }
+        }
+
+
+        // GET: VolunteerOpportunity/Delete/5
+        [HttpGet]
+        public ActionResult DeleteConfirm(int id)
+        {
+            string url = "volunteeropportunitydata/findvolunteeropportunity/" + id;
+            HttpResponseMessage response = client.GetAsync(url).Result;
+            if (response.IsSuccessStatusCode)
+            {
+
+                VolunteerOpportunityDto SelectedVolunteerOpportunity = response.Content.ReadAsAsync<VolunteerOpportunityDto>().Result;
+                return View(SelectedVolunteerOpportunity);
+            }
+            else
+            {
+                return RedirectToAction("Error");
+
+            }
+        }
+
+
+        // POST: VolunteerOpportunity/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken()]
+        public ActionResult Delete(int id)
+        {
+            string url = "volunteeropportunitydata/deletevolunteeropportunity/" + id;
+            HttpContent content = new StringContent("");
+            HttpResponseMessage response = client.PostAsync(url, content).Result;
+            if (response.IsSuccessStatusCode)
+            {
+                return RedirectToAction("List");
+            }
+            else
+            {
+                return RedirectToAction("Error");
+            }
+        }
+
+        public ActionResult Error()
+        {
+            return View();
+        }
+    }
+}
